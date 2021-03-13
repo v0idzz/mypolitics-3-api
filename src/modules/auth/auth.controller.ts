b1e3a8e95c342e3b141e558b0097e1686da0d3fb
Adrian Orłów <adrian@orlow.me>
2021-03-13 15:10:44 +0100

    hotfix

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get, UseGuards, HttpStatus, Req, Res, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  HttpStatus,
+  Req,
+  Res,
+  Query,
+  Post,
+  BadRequestException,
+  Body
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
 import Cookies from 'cookies';
-import { Cookies as ConstCookies } from '../../constants';
+import { Cookies as ConstCookies, ErrorsMessages } from '../../constants';
 import dayjs from 'dayjs';
 import { AuthService } from './auth.service';
 import { User } from '../users/entities/user.entity';
+import { ErrorCode } from '../../types';
+import { ActionTokenType } from './enums/action-token-type.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -48,5 +61,62 @@ export class AuthController {
     if (verified) {
       res.redirect('/auth?state=verified');
     }
+  }
+
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  async login(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() { recaptcha }: {
+      recaptcha: string;
+    }
+  ): Promise<any> {
+    const cookies = new Cookies(req, res);
+    const user = req.user as User;
+
+    const captchaValid = await this.authService.validateReCaptcha(recaptcha);
+    if (!captchaValid) {
+      throw new BadRequestException();
+    }
+
+    if (!user.emailVerified) {
+      const lastToken = await this.authService.actionTokensService.findOne(
+        { user },
+        {},
+        { sort: 1 },
+      );
+      const lastTokenValid = dayjs(lastToken?.expiresOn).isAfter(dayjs());
+
+      if (lastTokenValid) {
+        throw new BadRequestException(ErrorsMessages[ErrorCode.EMAIL_NOT_VERIFIED_SENT_IN_15_MIN]);
+      }
+
+      const token = await this.authService.createAndGetActionToken(user, ActionTokenType.VERIFY_EMAIL);
+      await this.authService.sendEmailConfirm(user, token);
+      throw new BadRequestException(ErrorsMessages[ErrorCode.EMAIL_NOT_VERIFIED_SENT_NOW]);
+    }
+
+    const token = await this.authService.createAndGetAccessToken(user);
+    cookies.set(ConstCookies.JWT, token, {
+      httpOnly: true,
+      expires: dayjs().add(7, 'day').toDate(),
+    });
+
+    res.status(200).end();
+  }
+
+  @Post('logout')
+  async logout(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<any> {
+    const cookies = new Cookies(req, res);
+    cookies.set(ConstCookies.JWT, '', {
+      httpOnly: true,
+      expires: dayjs().toDate(),
+    });
+
+    res.status(200).end();
   }
 }
--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -1,7 +1,9 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { ErrorsMessages } from '../../../constants';
+import { ErrorCode } from '../../../types';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -12,8 +14,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(username: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(username, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new BadRequestException(ErrorsMessages[ErrorCode.WRONG_CREDENTIALS]);
     }
+
     return user;
   }
 }
