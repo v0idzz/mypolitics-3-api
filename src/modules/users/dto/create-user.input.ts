7da4eeb74434884a68a60737670003a687c7c0cf
Piotr Romanowski <wsrh456@gmail.com>
2021-04-15 15:30:32 +0200

    Revert "Release v3.0.1"
    This reverts commit 98b9cbf038505cb4b7a5e97c97ceb6085ff9af86.

--- src/modules/users/dto/create-user.input.ts
+++ src/modules/users/dto/create-user.input.ts
@@ -1,19 +1,14 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsEmail, Length, Matches, MaxLength, MinLength } from 'class-validator';
+import { QuizVersion } from '../../quiz-versions/entities/quiz-version.entity';
 
 @InputType()
 export class CreateUserInput {
-  @Length(2, 69)
   @Field(() => String)
   name: string;
 
-  @MaxLength(420)
-  @IsEmail()
   @Field(() => String)
   email: string;
 
-  @MinLength(6)
-  @Matches(/[A-Z]/)
   @Field(() => String)
   password: string;
 
